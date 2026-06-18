cadae471cfbaba845151425b512483c52474451f
hmgene <human.gim@gmail.com>
2026-06-18 10:41:33 -0400

    repaiir_to_selfrenewal

--- switch.sh
+++ switch.sh
@@ -1,8 +1,13 @@
+
+
+
 for f in *.{r,sh}; do
     # 1. Edit the file and save the output to a temp file
-    sed -e "s/good_repair/high_selfrenewal/g" \
+    [ "$f" = "${0##*/}" ] && continue
+    sed -E -e "s/good_repair/high_selfrenewal/g" \
         -e "s/bad_repair/low_selfrenewal/g" \
-        -e "s/repairment/selfrenewal/g" "$f" > "$f.tmp"
+        -e "s/repair(e)?ment/selfrenewal/g" \
+        "$f" > "$f.tmp"
         
     # 2. Overwrite the original file with the temporary one
     #mv "$f.tmp" "$f"
